5048a0ff6cd82b792318ff7bc56f938a7759bf69
Zu-shi <accountistaken@gmail.com>
2023-08-23 01:25:14 -0400

    13.7 add error handler

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,19 +2,18 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
-router.get('/api/blogs', async (req, res) => {
+router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/api/blogs', async (req, res) => {
+router.post('/', async (req, res, next) => {
   console.log(req.body)
   try {
     const blog = await Blog.create(req.body) // note that it saves automatically!
     res.json(blog)
   } catch (error) {
-    console.log(error)
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
@@ -23,7 +22,7 @@ const BlogFinder = async (req, res, next) => {
   next()
 }
 
-router.delete('/api/blogs/:id', async (req, res) => {
+router.delete('/:id', BlogFinder, async (req, res) => {
   if (req.blog) {
     //await Blog.destroy({ where: { id: req.params.id } }) // note that it saves automatically!
     await req.blog.destroy()
@@ -34,22 +33,37 @@ router.delete('/api/blogs/:id', async (req, res) => {
   }
 })
 
-router.put('/api/blogs/:id', async (req, res) => {
+router.put('/:id', BlogFinder, async (req, res, next) => {
   if (req.blog) {
-    note.likes = req.body.likes
-    await req.blog.save()
+    blog.likes = req.body.likes
+    try {
+      await req.blog.save()
+      res.json(req.blog)
+    }
+    catch (error) {
+      next(error)
+    }
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.get('/:id', BlogFinder, async (req, res) => {
+  if (req.blog) {
     res.json(req.blog)
   } else {
     res.status(404).end()
   }
 })
 
-router.get('/api/blogs/:id', async (req, res) => {
-  if (req.blog) {
-    res.json(req.blog)
-  } else {
-    res.status(404).end()
-  }
-})
+const errorHandler = (error, request, response, next) => {
+  console.log("error handler evoked")
+  console.error(error.message)
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+router.use(errorHandler)
 
 module.exports = router;
