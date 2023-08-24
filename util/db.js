0194172ecbc449b3915662f9fead998642a90a9f
Zu-shi <accountistaken@gmail.com>
2023-08-24 18:52:53 -0400

    13.17 convert database setyo to migrations

--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -13,6 +14,7 @@ const sequelize = new Sequelize(DATABASE_URL, {
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('connected to the database')
   } catch (err) {
     console.log('failed to connect to the database')
@@ -23,4 +25,37 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { connectToDatabase, sequelize }
+
+const queryInterface = sequelize.getQueryInterface();
+
+if (!queryInterface) {
+  console.log('Failed to get queryInterface');
+  process.exit(1);
+}
+
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js', // interesting! imported as globs
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(), // program to run the migrations
+  logger: console, // logging storage
+}
+
+const runMigrations = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+
+  console.log('Migraitions up to date', {
+    files: migrations.map((mig) => mig.name)
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
