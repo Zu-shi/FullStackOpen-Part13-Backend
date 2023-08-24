69bd24fff02af412ae025d249727115a5b3f3ee4
Zu-shi <accountistaken@gmail.com>
2023-08-24 00:26:00 -0400

    13.8 add basic user functionality

---
+++ controllers/users.js
@@ -0,0 +1,42 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res, next) => {
+  const result = await User.findAll()
+  res.json(result)
+})
+
+router.get('/:id', async (req, res, next) => {
+  const result = await User.findByPk(req.params.id)
+  res.json(result)
+})
+
+router.put('/:username', async (req, res, next) => {
+  const user = await User.find({ where: { id: req.body.id } })
+  try {
+    user.username = req.params.username
+    await user.save()
+    res.json(user)
+  } catch (error) { next(error) }
+})
+
+router.post('/', async (req, res, next) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    res.json(user)
+  } catch (error) { next(error) }
+})
+
+const errorHandler = (error, request, response, enxt) => {
+  console.log("user error handler evoked")
+  console.error(error.message)
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+router.use(errorHandler)
+
+module.exports = router;
