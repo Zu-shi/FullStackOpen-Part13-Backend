1a97695c8456fadeaea02f6189f47ab13ec99b48
Zu-shi <accountistaken@gmail.com>
2023-08-24 20:36:10 -0400

    13.19 add reading list

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readinglist')
 
 Blog.belongsTo(User)
 User.hasMany(Blog)
 
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
+
 module.exports = {
   Blog,
-  User
+  User,
+  ReadingList
 }
